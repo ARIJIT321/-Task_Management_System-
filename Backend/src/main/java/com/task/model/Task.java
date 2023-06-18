7876c015104747e8f0c26f7499e0e1dca422fff9
Arijit Bhatta <arijitbhatta123@gmail.com>
2023-06-18 20:20:05 +0530

    created model classes

---
+++ Backend/src/main/java/com/task/model/Task.java
@@ -0,0 +1,33 @@
+package com.task.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private LocalDate dueDate;
+
+    private boolean completed;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User assignedUser;
+}
---
+++ Backend/src/main/java/com/task/model/User.java
@@ -0,0 +1,22 @@
+package com.task.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+}
