bc78f0eee924c3557c00e1718937a546bd614a7f
Arijit Bhatta <arijitbhatta123@gmail.com>
2023-06-19 11:11:08 +0530

    adding repository classes

---
+++ Backend/src/main/java/com/task/repo/TaskRepository.java
@@ -0,0 +1,21 @@
+package com.task.repo;
+
+import com.task.model.Task;
+import com.task.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+@Repository
+public interface TaskRepository extends JpaRepository<Task,Long> {
+
+    List<Task> findByTitleContainingIgnoreCase(String title);
+    List<Task> findByAssignedUser(User assignedUser);
+
+    List<Task> findByDescription(String description);
+
+    List<Task> findByDueDate(LocalDate dueDate);
+
+    List<Task> findByCompleted(Boolean completed);
+}
